5922be42e3e5854cca88b71274ca6cc4dc06cbbf
bryan torres <bryannahum10gmail.com>
2019-05-29 17:22:24 -0500

    update package.json, procfile, and hello.js PORT

--- Pages.js
+++ Pages.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function (req,res) {
 	var q = url.parse(req.url, true);
@@ -14,6 +15,6 @@ http.createServer(function (req,res) {
 	res.write(data);
 	return res.end();
 });
-}).listen(8080);
+}).listen(PORT);
 
 console.log('server listening on port 8080...')
